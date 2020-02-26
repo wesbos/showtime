72c3613416a63786572fb586a30a4f420867e122
Wes Bos <wesbos@gmail.com>
2020-02-26 10:46:26 -0500

    data

--- src/http/get-scrape/index.js
+++ src/http/get-scrape/index.js
@@ -1,39 +1,60 @@
-// Enable secure sessions, express-style middleware, and more:
-// https://docs.begin.com/en/functions/http/
-//
-// let begin = require('@architect/functions')
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+// const youtubedl = require('youtube-dl');
+const data = require('@begin/data');
 
-let html = `
-<!doctype html>
-<html lang=en>
-  <head>
-    <meta charset=utf-8>
-    <title>Hi!</title>
-    <link rel="stylesheet" href="https://static.begin.app/starter/default.css">
-    <link href="data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=" rel="icon" type="image/x-icon" />
-  </head>
-  <body>
-    <h1 class="center-text">
-      Hello world!
-    </h1>
-    <p class="center-text">
-      Your new route is ready to go!
-    </p>
-    <p class="center-text">
-      Learn more about building <a href="https://docs.begin.com/en/functions/http/" class="link" target="_blank">Begin HTTP functions here</a>.
-    </p>
-  </body>
-</html>
-`
+const url = `https://www.hgtv.ca/videos/`;
+
+async function getRecentShows() {
+  console.log('Getting shows');
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  await page.goto(url);
+  const shows = await page.evaluate(() => {
+    const drawer = document.querySelector('.shawVideo_drawer');
+    const els = Array.from(drawer.querySelectorAll('li'));
+    const shows = els.map(el => ({
+      series: el.querySelector('[class*=title]').textContent,
+      title: el.querySelector('[class*=episodeName]').textContent,
+      season: el.querySelector('[class*=season]').textContent,
+      episode: el.querySelector('[class*=_footer] [class*=_episode]')
+        .textContent,
+      url: window.location.origin + el.dataset.video_url,
+      id: el.dataset.video_id,
+      scrapeDate: Date.now(),
+    }));
+    return shows;
+  });
+
+  shows.forEach(async show => {
+    // check if we have it
+    const existingShow = await data.get({ table: 'shows', key: show.id });
+    if (existingShow) return;
+    await data.set({
+      table: 'shows',
+      key: show.id,
+      show,
+    });
+  });
+
+  await browser.close();
+  return shows;
+}
 
 // HTTP function
 exports.handler = async function http(req) {
-  console.log(req)
+  getRecentShows();
+  // const shows = await getRecentShows();
+  const shows = await data.get({ table: 'shows' });
+  console.log(shows);
+  const html = `hey`;
   return {
     headers: {
       'content-type': 'text/html; charset=utf8',
-      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
+      'cache-control':
+        'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0',
     },
-    body: html
-  }
-}
+    body: JSON.stringify(shows),
+  };
+};
